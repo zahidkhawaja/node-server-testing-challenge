84be8daaaaea983a114da8231c89323d4164ecc3
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-23 16:11:48 -0400

    Add server test suite (not passing yet, just setup)

--- api/server.js
+++ api/server.js
@@ -1,11 +1,28 @@
 const express = require("express");
 
+const Computers = require("../computers/computersModel");
+
 const server = express();
 
 server.use(express.json());
 
 server.get("/", (req, res) => {
     res.status(200).json({ message: "API is up"})
+})
+
+// GET all computers
+server.get("/computers", (req, res) => {
+
+})
+
+// POST new computer
+server.post("/computers", (req, res) => {
+
+})
+
+// DELETE a computer by ID
+server.delete("/computers", (req, res) => {
+    
 })
 
 module.exports = server;
